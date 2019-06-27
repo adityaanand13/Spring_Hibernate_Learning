fd18e0750d9a260f6157ae123ed690f24f7193f3
Aditya Anand <anand.aditya.anand@gmail.com>
2019-06-27 23:30:08 +0530

    Basic of Inversion of control on POJO

---
+++ spring_core/src/com/Rough_InversionOfControl/TrackCoach.java
@@ -0,0 +1,9 @@
+package com.Rough_InversionOfControl;
+
+public class TrackCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run 5kms Daily";
+    }
+}
