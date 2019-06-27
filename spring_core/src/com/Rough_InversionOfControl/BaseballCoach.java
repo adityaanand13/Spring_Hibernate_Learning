24a6369601f7749700658890956cff8d0ed428cf
Aditya Anand <anand.aditya.anand@gmail.com>
2019-06-27 23:28:38 +0530

    basic of Inversion of control

---
+++ spring_core/src/com/Rough_InversionOfControl/BaseballCoach.java
@@ -0,0 +1,9 @@
+package com.Rough_InversionOfControl;
+
+public class BaseballCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout(){
+        return "30 minutes on batting practise";
+    }
+}
