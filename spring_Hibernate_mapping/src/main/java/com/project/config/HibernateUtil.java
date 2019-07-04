e1d50cd31791ecbc21f7c719dda588c024d9e6ef
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-04 23:27:37 +0530

    Setup Config files

---
+++ spring_Hibernate_mapping/src/main/java/com/project/config/HibernateUtil.java
@@ -0,0 +1,53 @@
+package com.project.config;
+
+
+import com.project.entity.Course;
+import com.project.entity.Instructor;
+import com.project.entity.InstructorDetail;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null){
+            try {
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+
+                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/hb_mapping?useSSL=false&serverTimezone=UTC");
+                properties.put(Environment.USER, "hbstudent");
+                properties.put(Environment.PASS, "hbstudent");
+                //verify mysql version for better dialect
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+                properties.put(Environment.SHOW_SQL, "true");
+
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                properties.put(Environment.HBM2DDL_AUTO, "update");
+
+                configuration.setProperties(properties);
+
+                configuration.addAnnotatedClass(Instructor.class);
+                configuration.addAnnotatedClass(InstructorDetail.class);
+                configuration.addAnnotatedClass(Course.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().
+                        applySettings(configuration.getProperties())
+                        .build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+}
