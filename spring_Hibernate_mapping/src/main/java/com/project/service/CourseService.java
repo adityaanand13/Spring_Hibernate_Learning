90bd838b90e3fbfe1878d26ea7ede4cd30330bae
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-05 23:43:37 +0530

    service ORM for Course entity

--- spring_Hibernate_mapping/src/main/java/com/project/service/CourseService.java
+++ spring_Hibernate_mapping/src/main/java/com/project/service/CourseService.java
@@ -3,8 +3,11 @@ package com.project.service;
 import com.project.config.HibernateUtil;
 import com.project.entity.Course;
 import com.project.entity.Instructor;
+import com.project.entity.Review;
+import com.project.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class CourseService {
 
@@ -63,6 +66,148 @@ public class CourseService {
             if (transaction != null) transaction.rollback();
             e.printStackTrace();
         }finally {
+            session.close();
+        }
+        return course;
+    }
+
+    public Course getById(int id){
+        // get the session
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = null;
+        Course course = null;
+        try {
+            //start the transaction
+            transaction = session.beginTransaction();
+            //get the object from session
+            course = session.get(Course.class, id);
+            //commit transaction
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }finally {
+            //close the session
+            session.close();
+        }
+        return course;
+    }
+
+    public void printById(int id){
+        // get the session
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = null;
+        Course course = null;
+        try {
+            //start the transaction
+            transaction = session.beginTransaction();
+            //get the object from session
+            course = session.get(Course.class, id);
+            System.out.println(course);
+            //commit transaction
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }finally {
+            //close the session
+            session.close();
+        }
+    }
+
+    public Course getWithReviews(int id){
+        // get the session
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = null;
+        Course course = null;
+        try {
+            //start the transaction
+            transaction = session.beginTransaction();
+            // load course from db
+            Query<Course> query = session.createQuery(
+                    "select c from Course c " +
+                            "JOIN FETCH c.reviews " +
+                            "WHERE c.id="+id,//add parameter in query
+                    Course.class
+            );
+            //execute query and get instructor
+            course = query.getSingleResult();
+
+            if (course==null)
+                System.out.println("Course with id: "+id+" not found");
+            //commit transaction
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }finally {
+            //close the session
+            session.close();
+        }
+        return course;
+    }
+
+    public Course addReview(int id, String reviewComment){
+        // get the session
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = null;
+        Course course = null;
+        try {
+            // start the transaction
+            transaction = session.beginTransaction();
+            // get the course by id
+            course = session.get(Course.class, id);
+
+            // update the session if exists in record
+            if (course != null){
+                //create new review
+                Review review = new Review(reviewComment);
+                course.addReview(review);
+                //save the review
+                session.save(review);
+            }
+            else
+                System.out.println("Instructor with id: "+id+" not found");
+
+            //commit transaction
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }finally {
+            //close the session
+            session.close();
+        }
+        return course;
+    }
+
+    public Course addStudent(int id, Student student){
+        // get the session
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = null;
+        Course course = null;
+        try {
+            // start the transaction
+            transaction = session.beginTransaction();
+            // get the course by id
+            course = session.get(Course.class, id);
+
+            // update the session if exists in record
+            if (course != null){
+                course.addStudent(student);
+                //save the review
+                session.save(student);
+            }
+            else
+                System.out.println("Instructor with id: "+id+" not found");
+
+            //commit transaction
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }finally {
+            //close the session
             session.close();
         }
         return course;
