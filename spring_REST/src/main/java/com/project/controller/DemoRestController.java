8ef1bbcfd6c2f1b0ad330d9a96aeeeed8386a849
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-06 23:46:09 +0530

    Demo Rest Controller

---
+++ spring_REST/src/main/java/com/project/controller/DemoRestController.java
@@ -0,0 +1,19 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class DemoRestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Home";
+    }
+}
